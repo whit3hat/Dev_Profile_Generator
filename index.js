41a5ccd4e2e463024e7088bfbb0a7d0935088a49
Spencer Wood <spencer.wood@me.com>
2020-02-09 22:11:22 -0600

    created questions for the username and color. Added axios varible

--- index.js
+++ index.js
@@ -1,8 +1,28 @@
+//required tools for application
 const inquirer = require('inquirer');
 const fs = require('fs');
 const util = require('util');
+const axios = require('axios');
+
+
+const writeFileAsync = util.promisify(fs.writeFile);
+
+const config = { headers: { accept: 'application/json' } };
+
+axios
+    .get()
 
 const questions = [
+    {
+        type: 'input',
+        name: 'github',
+        message: 'What is your GitHub Username?'
+    },
+    {
+        type: 'input',
+        name: 'color',
+        message: 'What is your favoriate color?'
+    }
   
 ];
 
