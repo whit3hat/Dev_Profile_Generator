905dab2411ecd94d12c1b79e3a27dfe9cd86ebcc
Spencer Wood <spencer.wood@me.com>
2020-02-14 13:24:57 -0600

    created new function to hold the questions and then pass to the axios call

--- index.js
+++ index.js
@@ -1,37 +1,57 @@
 const inquirer = require('inquirer');
 const axios = require('axios');
 
-let output = [];
+// let output = [];
+// console.log(output.keys(0));
 
-const questions = [
-  {
-      name: 'color',
-      type: 'input',
-      message: 'What is your favorite color? (Red, Blue, Green or Pink)'
-  },
-  {
-      name: 'github',
-      type: 'input',
-      message: ' What is your Github username?'
-  },
-];
+// const questions = [
+//   {
+//       name: 'color',
+//       type: 'input',
+//       message: 'What is your favorite color? (Red, Blue, Green or Pink)'
+//   },
+//   {
+//       name: 'github',
+//       type: 'input',
+//       message: ' What is your Github username?'
+//   },
+// ];
 
-function ask(){
-    inquirer.prompt(questions).then(answers => {
-        output.push(answers);
-        console.log(output);
-        gitRequest();
-    })
-};
+// function ask(){
+//     inquirer.prompt(questions).then(answers => {
+//         output.push(answers);
+        
+//         gitRequest();
+//     })
+// };
+gitRequest();
 
- async function gitRequest() {
-            let res = await axios.get('https://api.github.com');
-            const data = res.data;
+async function gitRequest() {
+            try {
+            const { github } = await inquirer.prompt({
+                message: 'What is your Github username?',
+                name: 'github'
+            });
+
+            const { color } = await inquirer.prompt({
+                message: 'What is your favorite color? (Red, Blue, Green or Pink)',
+                name: 'color'
+            });
+
+            const { data } = await axios.get(
+                `https://api.github.com/user/${github}`
+            );
+
             console.log(data);
-    };
 
-ask();
+                   
 
+        } catch (err) {
+            console.log(err);
+        }
+
+
+}
 // function writeToFile(fileName, data) {
  
 // }
