72dabaacf001ad46f7940224dcb9c53392622ab8
Spencer Wood <spencer.wood@me.com>
2020-02-14 13:38:27 -0600

    Deleted the old function that is no longer used. currently it will ask questions but not pass to the axios request currently

--- index.js
+++ index.js
@@ -1,29 +1,6 @@
 const inquirer = require('inquirer');
 const axios = require('axios');
 
-// let output = [];
-// console.log(output.keys(0));
-
-// const questions = [
-//   {
-//       name: 'color',
-//       type: 'input',
-//       message: 'What is your favorite color? (Red, Blue, Green or Pink)'
-//   },
-//   {
-//       name: 'github',
-//       type: 'input',
-//       message: ' What is your Github username?'
-//   },
-// ];
-
-// function ask(){
-//     inquirer.prompt(questions).then(answers => {
-//         output.push(answers);
-        
-//         gitRequest();
-//     })
-// };
 gitRequest();
 
 async function gitRequest() {
@@ -31,8 +8,8 @@ async function gitRequest() {
             const { github } = await inquirer.prompt({
                 message: 'What is your Github username?',
                 name: 'github'
-            });
-
+              });
+              
             const { color } = await inquirer.prompt({
                 message: 'What is your favorite color? (Red, Blue, Green or Pink)',
                 name: 'color'
@@ -43,7 +20,7 @@ async function gitRequest() {
             );
 
             console.log(data);
-
+            
                    
 
         } catch (err) {
